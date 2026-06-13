07b4073657b0207f30b5222d5f45008a45551366
Julian Suszko <s33103@pjwstk.edu.pl>
2026-06-13 16:49:21 +0200

    Change of token id in model

--- Domain/Entities/Token.cs
+++ Domain/Entities/Token.cs
@@ -2,10 +2,9 @@ namespace Domain.Entities;
 
 public class Token
 {
-    public int TokenId { get; set; }
-    public string RefreshTokenValue { get; set; } = string.Empty;
-    public DateTime ExpirationDate { get; set; }
+    public Guid UserId { get; set; } 
+    public string RefreshToken { get; set; } = string.Empty;
+    public DateTime ExpirationDate { get; set; } 
 
-    public Guid UserId { get; set; }
     public virtual User User { get; set; } = null!;
 }
