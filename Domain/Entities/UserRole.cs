7fdb229d9c62ec0bc29856aa5c8a52bb0608e848
Julian Suszko <s33103@pjwstk.edu.pl>
2026-06-13 20:01:31 +0200

    Minor fixes in project configuration and in userrole entity

--- Domain/Entities/UserRole.cs
+++ Domain/Entities/UserRole.cs
@@ -3,7 +3,7 @@ namespace Domain.Entities;
 public class UserRole
 {
     public int RoleId { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     public virtual ICollection<User> Users { get; set; } = new List<User>();
 }
