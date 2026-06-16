ebe9a109b7f724e72e4a5fb17754211743deb299
Julian Suszko <s33103@pjwstk.edu.pl>
2026-06-16 17:56:38 +0200

    Add testing project to the solution

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Application.Subscription;
+using Microsoft.EntityFrameworkCore;
+
+namespace Tests;
+
+public class UnitTest1
+{
+
+    public UnitTest1()
+    {
+
+    }
+    
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        
+        //Act
+    }
+}
