56249deba28ad8ba32f056596ec0ec9815788db1
Julian Suszko <s33103@pjwstk.edu.pl>
2026-06-16 12:14:19 +0200

    make auth work in swaggerui

--- API/Program.cs
+++ API/Program.cs
@@ -1,28 +1,59 @@
 using API.Exceptions;
+using Application;
 using Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
-using Application;
 using Application.Authentication;
+using Infrastructure;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.OpenApi;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<DatabaseContext>(options =>
+builder.Services.AddOpenApi(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    options.AddDocumentTransformer((document, context, ct) =>
+    {
+        document.Components ??= new OpenApiComponents();
+        document.Components.SecuritySchemes = new Dictionary<string, OpenApiSecurityScheme>
+        {
+            [JwtBearerDefaults.AuthenticationScheme] = new OpenApiSecurityScheme()
+            {
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "Json Web Token",
+                In = ParameterLocation.Header,
+                Description = "Please insert JWT token into field",
+                Name = "Authorization",
+                Scheme = JwtBearerDefaults.AuthenticationScheme,
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = JwtBearerDefaults.AuthenticationScheme },
+            }
+        };
+        
+        foreach (var operation in document.Paths.Values.SelectMany(path => path.Operations))
+        {
+            operation.Value.Security.Add(new OpenApiSecurityRequirement
+            {
+                            
+                [new OpenApiSecurityScheme { Reference = new OpenApiReference { Id = "Bearer", Type = ReferenceType.SecurityScheme } }] = Array.Empty<string>()
+            });
+        }
+        
+        return Task.CompletedTask;
+    });
 });
 
-builder.Services.AddProblemDetails();
-builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
 builder.Services.AddApplication();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
+builder.Services.AddJwtAuthentication(builder.Configuration);
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -30,8 +61,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.UseSwaggerUI(opt => opt.SwaggerEndpoint("/openapi/v1.json", "API v1"));
 }
-
+app.UseExceptionHandler();
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 
