dd1e5d9a75e45fd3f1fc643a02156daa934d5418
Julian Suszko <s33103@pjwstk.edu.pl>
2026-06-16 17:00:39 +0200

    divide payment

--- Domain/Entities/ContractPayment.cs
+++ Domain/Entities/ContractPayment.cs
@@ -1,16 +1,14 @@
 namespace Domain.Entities;
 
-public class Payment
+public class ContractPayment
 {
     public int PaymentId { get; set; }
     public decimal Amount { get; set; }
     public DateTime PaymentDate { get; set; }
     public int ClientId { get; set; }
 
-    public int? ContractId { get; set; }
-    public int? SubscriptionId { get; set; }
+    public int ContractId { get; set; }
     
     public virtual Client Client { get; set; } = null!;
-    public virtual Contract? Contract { get; set; }
-    public virtual Subscription? Subscription { get; set; }
+    public virtual Contract Contract { get; set; }
 }
